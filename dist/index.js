afb5798326e1659e87c5d5739d3d921988d4dc67
Shuki Levy <levy.shuki@gmail.com>
2021-05-31 15:18:18 +0300

    add dist

---
+++ dist/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const test_1 = require("./test");
+console.log("Hello World");
+test_1.test();
---
+++ dist/test.js
@@ -0,0 +1,8 @@
+"use strict";
+// export {run} from '@oclif/command'
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.test = void 0;
+const test = () => {
+    console.log('Hello Test');
+};
+exports.test = test;
