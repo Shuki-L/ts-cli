97b32707c571a8884bd7f4fb39de09474960b03d
Shuki Levy <levy.shuki@gmail.com>
2021-05-31 15:17:27 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+import { test } from "./test";
+console.log("Hello World");
+
+test();
---
+++ src/test.ts
@@ -0,0 +1,13 @@
+// export {run} from '@oclif/command'
+
+
+const test = () => {
+    console.log('Hello Test');
+
+}
+
+
+
+export {
+    test
+}
